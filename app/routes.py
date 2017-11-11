035c7de78ac072dc1a09bd739ffac33e760a1e1f
HalfMomentum <mm305@snu.edu.in>
2017-11-11 23:23:15 +0000

    next: integrate db

--- app/routes.py
+++ app/routes.py
@@ -2,64 +2,36 @@ from flask import render_template, flash, request, redirect, url_for, session
 import auth, json
 from app import app,database
 
+
 @app.route('/')
-def home():
-    db = database.get_db()
-    cur = db.execute('select name,email from Users')
-    users = cur.fetchall()
-    return render_template('home.html', users=users)
-
-@app.route('/home')
+@app.route('/index')
 def index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('login'))
+    return render_template('index.html',title="Landing")
 
-    access_token = access_token[0]
-    from urllib2 import Request, urlopen, URLError
-
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                  None, headers)
-    try:
-        res = urlopen(req)
-    except URLError, e:
-        if e.code == 401:
-            # Unauthorized - bad token
-            session.pop('access_token', None)
-            return redirect(url_for('login'))
-        return render_template(
-            'index.html',
-            msg = json.loads(res.read()),
-            title='Detail',
-        )
-
-    msg = json.loads(res.read())
-    print(msg['id'])
-    return render_template(
-        'index.html',
-        msg = msg,
-        title='Detail',
-    )
-
-
-@app.route('/add', methods=['POST'])
-def add_user():
-    db = database.get_db()
-    db.execute('insert into Users (name, email) values (?, ?)',
-               [request.form['name'], request.form['email']])
-    db.commit()
-    flash('New User registered successfully')
-    return redirect(url_for('home'))
+@app.route('/status/<real>')
+def status(real=True):
+    return render_template('status.html',count=2,current=real, title='Live')
 
 @app.route('/logout')
 def logout():
-    return redirect(url_for('home'))
+    return redirect(url_for('index'))
 
-@app.route('/login')
+@app.route('/login', methods=['POST','GET'])
 def login():
-    callback=url_for('authorized', _external=True)
-    return auth.google.authorize(callback=callback)
+    if request.method == 'GET':
+        return render_template('login.html',title='Login')
+    else:
+        db = database.get_db()
+        Name = request.form['name']
+        Email = request.form['email']
+        if len(Name)>0 and len(Email)>0:
+            db.execute('insert into Users (name, email) values (?, ?)',
+                       [Name,Email])
+            db.commit()
+            flash('New User registered successfully')
+            return redirect(url_for("index"))
+        flash('Wrong Credentials')
+        return redirect(url_for('login'))
 
 
 def main():
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+extensions = ['sphinxcontrib.autohttp.flask']
+DATABASE = '/tmp/schema.sql'
+WTF_CSRF_ENABLED = True
+GOOGLE_CLIENT_ID = '716230243314-k9j5b63dook0mvsehusfondeo9q5rp89.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'XNPhyKJjmbKIa1iSihhzNWcU'
+SECRET_KEY = 'ufoyRofNuej25JEAR/wrdTUQLejwyiggEupicE'
